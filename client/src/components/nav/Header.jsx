37417880a97b53a3b7cdc03d21d374c999dbc25f
Akshit Patel <aksh1150@gmail.com>
2020-10-24 12:02:08 -0400

    Create login and register menu

--- client/src/components/nav/Header.jsx
+++ client/src/components/nav/Header.jsx
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
-import { MailOutlined, SettingOutlined } from "@ant-design/icons";
+import {
+  AppstoreOutlined,
+  SettingOutlined,
+  UserAddOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 const { SubMenu } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState("");
+  const [current, setCurrent] = useState("home");
 
-  const handleClick = () => {
-    //
+  const handleClick = (e) => {
+    setCurrent(e.key);
   };
 
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-      <Menu.Item key="mail" icon={<MailOutlined />}>
+      <Menu.Item key="home" icon={<AppstoreOutlined />}>
         Home
       </Menu.Item>
 
-      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Register">
+      <Menu.Item key="login" icon={<UserOutlined />}>
+        Login
+      </Menu.Item>
+
+      <Menu.Item key="register" icon={<UserAddOutlined />}>
+        Register
+      </Menu.Item>
+
+      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="User">
         <Menu.Item key="setting:1">Option 1</Menu.Item>
         <Menu.Item key="setting:2">Option 2</Menu.Item>
       </SubMenu>
