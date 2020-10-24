7534732da0e7bfcd120f457e8cb729091f63428f
Akshit Patel <aksh1150@gmail.com>
2020-10-24 18:33:58 -0400

    Add firebase configuration

---
+++ client/src/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA9M-XjPigltsVLeVhwHz9yaBFNPvS8rPE",
+  authDomain: "ecom-20a23.firebaseapp.com",
+  databaseURL: "https://ecom-20a23.firebaseio.com",
+  projectId: "ecom-20a23",
+  storageBucket: "ecom-20a23.appspot.com",
+  messagingSenderId: "1088694565396",
+  appId: "1:1088694565396:web:e2e333dfac6c764a0a0501",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
