d0b742f1b96e3f60947aee8a3a4ac4467df0b50c
Akshit Patel <aksh1150@gmail.com>
2020-10-25 16:15:20 -0400

    Create compelete registration page

---
+++ client/src/pages/auth/RegisterComplete.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+
+const RegisterComplete = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [isNoti, setNoti] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const config = {
+      url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
+      handleCodeInApp: true,
+    };
+
+    await auth.sendSignInLinkToEmail(email, config);
+    setNoti(true);
+    window.localStorage.setItem("emailForRegistration", email);
+    // setEmail("");
+  };
+  const notiMessage = () => (
+    <div className="alert alert-success mt-4" role="alert">
+      Email sent to {email}. Click the link to complete your registration.
+    </div>
+  );
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
+      <button type="submit" className="btn btn-raised btn-primary mt-4">
+        Register
+      </button>
+      {isNoti ? notiMessage() : ""}
+    </form>
+  );
+
+  return (
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-2">
+          <h4>Register</h4>
+          {registerForm()}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default RegisterComplete;
