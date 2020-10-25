73045cd5b904c45500f7a56441694f55479e2489
Akshit Patel <aksh1150@gmail.com>
2020-10-25 17:35:04 -0400

    import redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import "antd/dist/antd.css";
 ReactDOM.render(
   <BrowserRouter>
