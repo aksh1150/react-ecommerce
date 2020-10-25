cbbb549dc4d8da0b5103acf02cf973c0fb4df4b2
Akshit Patel <aksh1150@gmail.com>
2020-10-25 16:25:26 -0400

    Add password link

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 
 import Header from "./components/nav/Header";
+import RegisterComplete from "./pages/auth/RegisterComplete";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/register/complete" component={RegisterComplete} />
       </Switch>
     </>
   );
--- client/src/pages/auth/RegisterComplete.jsx
+++ client/src/pages/auth/RegisterComplete.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
 
 const RegisterComplete = ({ history }) => {
   const [email, setEmail] = useState("");
   const [isNoti, setNoti] = useState(false);
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    setEmail(window.localStorage.getItem("emailForRegistration"));
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,22 +23,24 @@ const RegisterComplete = ({ history }) => {
     // setEmail("");
   };
   const notiMessage = () => (
-    <div className="alert alert-success mt-4" role="alert">
+    <div className="alert alert-success mt-4 p-2" role="alert">
       Email sent to {email}. Click the link to complete your registration.
     </div>
   );
 
-  const registerForm = () => (
-    <form onSubmit={handleSubmit}>
+  const completeRegisterForm = () => (
+    <form onSubmit={handleSubmit} className="mt-4">
+      <input type="email" className="form-control" value={email} disabled />
       <input
-        type="email"
-        className="form-control"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        type="password"
+        className="form-control mt-4 p-2"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Enter your password"
         autoFocus
       />
       <button type="submit" className="btn btn-raised btn-primary mt-4">
-        Register
+        Complete Registeration
       </button>
       {isNoti ? notiMessage() : ""}
     </form>
@@ -44,8 +50,8 @@ const RegisterComplete = ({ history }) => {
     <div className="container p-4">
       <div className="row">
         <div className="col-md-6 offset-md-2">
-          <h4>Register</h4>
-          {registerForm()}
+          <h4>Register Complete</h4>
+          {completeRegisterForm()}
         </div>
       </div>
     </div>
