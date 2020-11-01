4b3c2b76e6fb016de0b3ba1bbb381102f4239fa5
Akshit Patel <aksh1150@gmail.com>
2020-11-01 12:02:08 -0500

    Create email field:

--- client/src/pages/auth/ForgotPassword.jsx
+++ client/src/pages/auth/ForgotPassword.jsx
@@ -6,7 +6,26 @@ const ForgotPassword = ({ history }) => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
 
-  return <div className="container col-md-6 offset-md-2 p-5">form</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="container col-md-6 offset-md-2 p-5">
+      {loading ? (
+        <h4 className="text-danger">Loading...</h4>
+      ) : (
+        <h4>Forgot Password</h4>
+      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default ForgotPassword;
