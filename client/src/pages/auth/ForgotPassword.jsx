12be67128d76abd8eb171a90f6d69ac8cd96f0f8
Akshit Patel <aksh1150@gmail.com>
2020-11-01 12:07:44 -0500

    Add config for firebase

--- client/src/pages/auth/ForgotPassword.jsx
+++ client/src/pages/auth/ForgotPassword.jsx
@@ -6,8 +6,15 @@ const ForgotPassword = ({ history }) => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+
+    const config = {
+      url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
+      handleCodeInApp: true,
+    };
+    await auth.sendPasswordResetEmail(email, config);
   };
   return (
     <div className="container col-md-6 offset-md-3 p-4">
