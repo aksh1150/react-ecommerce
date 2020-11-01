ee46cbd0d2dec9dc8283c6cafd5a10b71ffccecb
Akshit Patel <aksh1150@gmail.com>
2020-11-01 11:58:31 -0500

    Create forgot password and add route to app

--- client/src/pages/auth/ForgotPassword.jsx
+++ client/src/pages/auth/ForgotPassword.jsx
@@ -1,3 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
 import { useSelector } from "react-redux";
+
+const ForgotPassword = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  return <div className="container col-md-6 offset-md-2 p-5">form</div>;
+};
+
+export default ForgotPassword;
